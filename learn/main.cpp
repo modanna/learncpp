fedca7c1c886b1cd95ab9088379a3175b8010510
ModAnna <modanna@protonmail.com>
2019-01-09 10:34:22 +0000

    init

---
+++ learn/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "learn.h"
+
+int main(int argc, char *argv[]){
+  int x1 = 10;
+  int x2 = 20;
+   std::cout << "Hello World!" << add(x1, x2) << std::endl;
+   return 0;
+}
+
